06d6c99d5ae706685c758179c255034d3b3e8c00
Prudhvi B <bnagasaiprudhvi4897@gmail.com>
2024-07-24 09:32:30 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,21 +1,31 @@
 package demo;
 
+import java.time.Duration;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.logging.Level;
-// import io.github.bonigarcia.wdm.WebDriverManager;
+//import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
+
 public class TestCases {
+    private static final String URL = "https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform";
     ChromeDriver driver;
+    Wrappers wrappers; 
 
     /*
      * TODO: Write your tests here with testng @Test annotation. 
@@ -49,11 +59,40 @@ public class TestCases {
         driver.manage().window().maximize();
     }
 
-    @AfterTest
-    public void endTest()
-    {
-        driver.close();
-        driver.quit();
+    @Test
+    public void testCase01() throws InterruptedException {  
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        Wrappers.navigateToUrl(driver, URL);
+        Wrappers.verifyThePage(driver, "forms");
 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        
+        WebElement nameElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='rFrNMe k3kHxc RdH0ib yqQS1 zKHdkd']/div/div/div/input")));
+        Wrappers.sendText(nameElement, "Crio Learner");
+        Wrappers.verifyText(nameElement, "Crio Learner");
+        
+        WebElement practicingAutomation = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//textarea[@class='KHxj8b tL9Q4c']")));
+        Wrappers.sendTextWithTime(practicingAutomation, "I want to be the best QA Engineer!");
+        Wrappers.verifyText(practicingAutomation, "I want to be the best QA Engineer!");
+        Wrappers.addExperience(driver, "3 - 5");
+        Wrappers.selectTools(driver, "Java");
+        Wrappers.selectTools(driver, "Selenium");
+        Wrappers.selectTools(driver, "TestNG");
+        Wrappers.selectAddress(driver, "Mr");
+        Thread.sleep(2000);
+        Wrappers.enterDate(driver);
+        Wrappers.enterTime(driver);
+        Wrappers.clickOnSubmitButton(driver);
+        Thread.sleep(1000);
+        Wrappers.successMessage(driver);
+    }
+
+    @AfterTest
+    public void endTest() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
+
+   
